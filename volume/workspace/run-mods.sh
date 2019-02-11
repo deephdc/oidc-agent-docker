2dfe1467f4e0e2dbe353d0ca6c2a0cec7de18549
giangzuzana <giang2005@gmail.com>
2019-02-11 20:41:15 +0100

    Update run-mods.sh

--- volume/workspace/run-mods.sh
+++ volume/workspace/run-mods.sh
@@ -6,5 +6,5 @@ export ORCHENT_TOKEN=$(oidc-token deep)
 orchent depcreate ./deep-oc-mods-mesos-cpu.yml \
 '{ "rclone_url": "https://nc.deep-hybrid-datacloud.eu/remote.php/webdav/",
    "rclone_vendor": "nextcloud",
-   "rclone_user": "DEEP-IAM-986b37db-bcb3-4067-8b03-d88dc4cc021d",
-   "rclone_pass": "dGMVmJuM60BPdJJ8Plr4Ogl1GJgVq7Ea6DwxJ-r40edA5o75RfhOXDefgyZj" }'
+   "rclone_user": "DEEP-IAM-id",
+   "rclone_pass": "passwd" }'
