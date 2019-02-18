59b4ca9b028fd0a7b97afd1aa2456d9c0cae9bb2
giangzuzana <giang2005@gmail.com>
2019-02-18 12:33:56 +0100

    ...

--- volume/workspace/run-mods-newtosca.sh
+++ volume/workspace/run-mods-newtosca.sh
@@ -3,5 +3,5 @@
 orchent depcreate ./deep-oc-mods_mesos-webdav-cpu.yml \
 '{ "rclone_url": "https://nc.deep-hybrid-datacloud.eu/remote.php/webdav/",
    "rclone_vendor": "nextcloud",
-   "rclone_user": "DEEP-IAM-986b37db-bcb3-4067-8b03-d88dc4cc021d",
-   "rclone_pass": "dGMVmJuM60BPdJJ8Plr4Ogl1GJgVq7Ea6DwxJ-r40edA5o75RfhOXDefgyZj" }'
+   "rclone_user": "$RCLONE_USER",
+   "rclone_pass": "$RCLONE_PASS" }'
