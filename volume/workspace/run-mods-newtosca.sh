ba499b39412bd5f8ac163e421df0b0842fdee358
giangzuzana <giang2005@gmail.com>
2019-02-18 12:26:17 +0100

    new tosca cpu gpu

--- volume/workspace/run-mods-newtosca.sh
+++ volume/workspace/run-mods-newtosca.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-orchent depcreate ./deep-oc-mods_mesos-cpu.yml \
+orchent depcreate ./deep-oc-mods_mesos-webdav-cpu.yml \
 '{ "rclone_url": "https://nc.deep-hybrid-datacloud.eu/remote.php/webdav/",
    "rclone_vendor": "nextcloud",
    "rclone_user": "DEEP-IAM-986b37db-bcb3-4067-8b03-d88dc4cc021d",
