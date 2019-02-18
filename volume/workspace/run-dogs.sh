6a4d01e8c20a61d0d92596031ba1e45ab626af17
giangzuzana <giang2005@gmail.com>
2019-02-18 09:44:41 +0100

    tosca for cpu and gpu

--- volume/workspace/run-dogs.sh
+++ volume/workspace/run-dogs.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-export ORCHENT_URL=https://paas.cloud.ba.infn.it/orchestrator
-export ORCHENT_TOKEN=$(oidc-token deep)
- 
 orchent depcreate ./deep-oc-dogs_breed-mesos-cpu.yml \
 '{ "rclone_url": "https://nc.deep-hybrid-datacloud.eu/remote.php/webdav/",
    "rclone_vendor": "nextcloud",
-   "rclone_user": "DEEP-IAM-id",
-   "rclone_pass": "passwd" }'
+   "rclone_user": "DEEP-IAM-986b37db-bcb3-4067-8b03-d88dc4cc021",
+   "rclone_pass": "dGMVmJuM60BPdJJ8Plr4Ogl1GJgVq7Ea6DwxJ-r40edA5o75RfhOXDefgyZj" }'
--- volume/workspace/run-mods.sh
+++ volume/workspace/run-mods.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-export ORCHENT_URL=https://paas.cloud.cnaf.infn.it/orchestrator
-export ORCHENT_TOKEN=$(oidc-token deep)
- 
-orchent depcreate ./deep-oc-mods-mesos-cpu.yml \
+orchent depcreate ./deep-oc-mods_mesos-cpu.yml \
+#orchent depcreate ./deep-oc-mods_mesos-webdav-cpu.yml \
 '{ "rclone_url": "https://nc.deep-hybrid-datacloud.eu/remote.php/webdav/",
    "rclone_vendor": "nextcloud",
-   "rclone_user": "DEEP-IAM-id",
-   "rclone_pass": "passwd" }'
+   "rclone_user": "DEEP-IAM-986b37db-bcb3-4067-8b03-d88dc4cc021d",
+   "rclone_pass": "dGMVmJuM60BPdJJ8Plr4Ogl1GJgVq7Ea6DwxJ-r40edA5o75RfhOXDefgyZj" }'
