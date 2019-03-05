0ae18b9cba7cf59406379c2ddae309a72d9ed72e
Stevo Dlugolinsky <stevo@dlugolinsky.com>
2019-03-05 20:40:00 +0100

    back to normal building state

---
+++ volume/workspace/run-mods-gpu-uisav.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+orchent depcreate ./tosca/deep-oc-mods_mesos-webdav-gpu-uisav.yml \
+'{
+	"docker_image": "deephdc/deep-oc-mods:gpu",
+	"rclone_url": "https://nc.deep-hybrid-datacloud.eu/remote.php/webdav/",
+	"rclone_vendor": "nextcloud",
+	"rclone_user": "'$RCLONE_USER'",
+	"rclone_pass": "'$RCLONE_PASS'"
+}'
