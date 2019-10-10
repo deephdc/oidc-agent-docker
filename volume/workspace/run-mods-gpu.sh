3276f2dba77b68708b8b6fb35b62cba124dce959
Stevo Dlugolinsky <stevo@dlugolinsky.com>
2019-10-10 14:53:48 +0200

    using deep-debug_log

--- volume/workspace/run-mods-gpu.sh
+++ volume/workspace/run-mods-gpu.sh
@@ -6,5 +6,8 @@ orchent depcreate ./tosca/deep-oc-mods_mesos-webdav-gpu.yml \
 	"rclone_url": "https://nc.deep-hybrid-datacloud.eu/remote.php/webdav/",
 	"rclone_vendor": "nextcloud",
 	"rclone_user": "'$RCLONE_USER'",
-	"rclone_password": "'$RCLONE_PASS'"
+	"rclone_password": "'$RCLONE_PASS'",
+	"num_cpus": "1",
+	"num_gpus": "1",
+	"run_command": "/srv/deep-debug_log/debug_log.sh --deepaas_port=$PORT0 --remote_dir=deepnc:/Logs/"
 }'
